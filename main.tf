d5ecec44208746e1481e8745b222ad9dfb31cbbf
vinceneil666 <trondjohnsen@hotmail.com>
2023-01-09 07:01:03 +0100

    ..

--- main.tf
+++ main.tf
@@ -2,25 +2,10 @@ resource "azurerm_resource_group" "apprg" {
   name     = "rg-app-services"
   location = "West Europe"
 }
-resource "azurerm_app_service_plan" "plan01" {
-  name                = "app-service-plan"
+resource "azurerm_service_plan" "my-app-plan" {
+  name                = "example"
+  resource_group_name = azurerm_resource_group.apprg.name
   location            = "West Europe"
-  resource_group_name = azurerm_resource_group.apprg.name
-
-  sku {
-    tier = "Standard"
-    size = "S1"
-  }
-}
-resource "azurerm_app_service" "app-service" {
-  name = "mywebapp"
-  location = "West Europe"
-  resource_group_name = azurerm_resource_group.apprg.name
-  app_service_plan_id = azurerm_app_service_plan.plan01.id
-  site_config {
-    dotnet_framework_version = "v4.0"
-  }
-  tags = {
-    environment = var.environment
-  }
+  os_type             = "Linux"
+  sku_name            = "P1v2"
 }
