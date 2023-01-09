43f2141e56014a667c9c679e70f63afa0a3735a4
vinceneil666 <trondjohnsen@hotmail.com>
2023-01-09 07:04:09 +0100

    added web app linux

--- main.tf
+++ main.tf
@@ -9,3 +9,11 @@ resource "azurerm_service_plan" "my-app-plan" {
   os_type             = "Linux"
   sku_name            = "P1v2"
 }
+resource "azurerm_linux_web_app" "example" {
+  name                = "mywebapp001"
+  resource_group_name = azurerm_resource_group.apprg.name
+  location            = "West Europe"
+  service_plan_id     = azurerm_service_plan.my-app-plan.id
+
+  site_config {}
+}
