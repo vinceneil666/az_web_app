e31493163e039dfe213c27e4ff05a020dd6fd14c
vinceneil666 <trondjohnsen@hotmail.com>
2023-01-09 06:27:06 +0100

    app plan

--- main.tf
+++ main.tf
@@ -6,3 +6,13 @@ resource "azurerm_resource_group" "example2" {
   name     = "example2"
   location = "West Europe"
 }
+resource "azurerm_app_service_plan" "example" {
+  name                = "example-appserviceplan"
+  location            = "West Europe"
+  resource_group_name = azurerm_resource_group.example.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
