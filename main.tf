29900a47f0d8be5d771da3cd2e94acc9c5510c4c
vinceneil666 <trondjohnsen@hotmail.com>
2023-01-09 07:24:33 +0100

    ..

--- main.tf
+++ main.tf
@@ -15,12 +15,6 @@ resource "azurerm_linux_web_app" "webapp001" {
   location            = "West Europe"
   service_plan_id     = azurerm_service_plan.my-app-plan.id
   site_config {
-    linux_fx_version = "DOTNETCORE|2.2"
-    #min_tls_version = "1.2"
-    always_on = true
-    scm_type = "None"
-    managed_pipeline_mode = "Integrated"
-    websockets_enabled = false
-    #use_32_bit_worker_process = true
+  minimum_tls_version = "1.2"
   }
 }
