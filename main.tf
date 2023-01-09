5728ca4bb003ab61c4d27e19e0e3b3a97d9ccc25
vinceneil666 <trondjohnsen@hotmail.com>
2023-01-09 06:23:47 +0100

    mer

--- main.tf
+++ main.tf
@@ -2,3 +2,7 @@ resource "azurerm_resource_group" "example" {
   name     = "example"
   location = "West Europe"
 }
+resource "azurerm_resource_group" "example2" {
+  name     = "example2"
+  location = "West Europe"
+}
