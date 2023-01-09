af5dfb9ecd48d4e126224f8b9c9cecb8a6be35d8
vinceneil666 <trondjohnsen@hotmail.com>
2023-01-09 07:09:06 +0100

    update of names

--- main.tf
+++ main.tf
@@ -3,13 +3,13 @@ resource "azurerm_resource_group" "apprg" {
   location = "West Europe"
 }
 resource "azurerm_service_plan" "my-app-plan" {
-  name                = "example"
+  name                = "appl-plan-01"
   resource_group_name = azurerm_resource_group.apprg.name
   location            = "West Europe"
   os_type             = "Linux"
   sku_name            = "P1v2"
 }
-resource "azurerm_linux_web_app" "example" {
+resource "azurerm_linux_web_app" "webapp001" {
   name                = "tj-01-mywebapp001"
   resource_group_name = azurerm_resource_group.apprg.name
   location            = "West Europe"
