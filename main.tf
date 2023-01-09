63b419efa46ea3a4543f2a88ffe522604206fe7c
vinceneil666 <trondjohnsen@hotmail.com>
2023-01-09 06:58:48 +0100

    added web app

--- main.tf
+++ main.tf
@@ -12,3 +12,15 @@ resource "azurerm_app_service_plan" "plan01" {
     size = "S1"
   }
 }
+resource "azurerm_app_service" "app-service" {
+  name = "mywebapp"
+  location = "West Europe"
+  resource_group_name = azurerm_resource_group.apprg.name
+  app_service_plan_id = azurerm_app_service_plan.plan01.id
+  site_config {
+    dotnet_framework_version = "v4.0"
+  }
+  tags = {
+    environment = var.environment
+  }
+}
